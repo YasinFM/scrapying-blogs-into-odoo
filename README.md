60ef09496ae5b7fc84ce9a19aaf422c04cda194a
maso <mostafa.barmshory@gmail.com>
2024-02-28 17:18:04 +0330

    [update] a comment on how to get list

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+To get list of all valid pages:
+
+    curl --location --globoff 'https://7tooti.com/api/v2/cms/term-taxonomies/205/contents?_px_fk=media_type&_px_fv=page&_px_p=0&_px_ps=500&_px_sk=modif_dtime&_px_so=d&graphql={counts%2Ccurrent_page%2Citems_per_page%2Cpage_number%2Citems{name}}'
+
+
--- scrape/run_spider.py
+++ scrape/run_spider.py
@@ -4,6 +4,7 @@ def run_scrapy_crawl():
     try:
         # Run the command in the terminal
         subprocess.run(["scrapy", "crawl", "blogSpider"], check=True)
+        # FIXME: replace with logger
         print("scrapy crawl blogSpider completed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error occurred while running scrapy crawl blogSpider: {e}")
